81efc8b2ab195420dbf1fcf8966c5cbee2432177
v3n_us 714 <venus.kabera@student.moringaschool.com>
2025-05-14 17:31:21 +0300

    updated nabar

--- frontend/src/Navbar/navbar.js
+++ frontend/src/Navbar/navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../AuthContext";
-import "../Navbar/navbar.css"; // Adjust the path as necessary
+import "../Navbar/navbar.css"; // Adjust path if needed
 
 function Navbar() {
   const { isAuthenticated, logout } = useAuth();
@@ -14,21 +14,22 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <Link to="/home">Home</Link>
-      <Link to="/cars">Cars</Link>
-      <Link to="/posts">Posts</Link>
-      <Link to="/about">About</Link>
-
-      {!isAuthenticated && (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </>
-      )}
-
-      {isAuthenticated && (
-        <button onClick={handleLogout}>Sign Out</button>
-      )}
+      <div className="navbar-links">
+        {isAuthenticated ? (
+          <>
+            <Link to="/home">Home</Link>
+            <Link to="/cars">Cars</Link>
+            <Link to="/posts">Posts</Link>
+            <Link to="/about">About</Link>
+            <button onClick={handleLogout} className="logout-button">Sign Out</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </>
+        )}
+      </div>
     </nav>
   );
 }
